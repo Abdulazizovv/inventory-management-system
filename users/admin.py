4629d3dc81c8e50a5b9c22e9d10372e3c70a1f9e
Ulugbek Abdulazizov <aqllibola567@gmail.com>
2025-07-08 14:28:43 +0500

    added user model to admin panel

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from users.models import User
 
-# Register your models here.
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    list_display = ('phone', 'full_name', 'role', 'is_staff', 'is_active', 'created_at')
+    list_filter = ('role', 'is_staff', 'is_active')
+    search_fields = ('phone', 'full_name')
+    ordering = ('-created_at',)
+
+    fieldsets = (
+        (None, {'fields': ('phone', 'password')}),
+        ('Personal Info', {'fields': ('full_name',)}),
+        ('Permissions', {'fields': ('role', 'is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Important dates', {'fields': ('last_login',)}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('phone', 'password1', 'password2', 'role', 'is_staff', 'is_active')}
+        ),
+    )
